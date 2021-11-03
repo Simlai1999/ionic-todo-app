d95c7f27c71e35700ecab6160fb89ca1b33a7b34
Sounak Simlai <sounaksimlai@gmail.com>
2021-11-03 17:38:59 +0530

    this is a small todo app made using ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AddNewTaskPage } from '../add-new-task/add-new-task.page';
+import { TodoService } from '../todo.service';
+import { UpdateTaskPage } from '../update-task/update-task.page';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todoList = []
 
-  constructor() {}
+  today: number = Date.now();
 
+  constructor(public modalCtlr: ModalController, public todoService: TodoService) {
+    this.getAllTask()
+  }
+
+  async addNewItem() {
+    const modal = await this.modalCtlr.create({
+      component: AddNewTaskPage,
+    })
+    modal.onDidDismiss().then(newTask => {
+      this.getAllTask()
+    })
+    return modal.present()
+  }
+
+  getAllTask() {
+    this.todoList = this.todoService.getAllTasks()
+  }
+
+  delete(key) {
+    this.todoService.deleteTask(key)
+    this.getAllTask()
+  }
+
+  async update(selectedTask) {
+    const modal = await this.modalCtlr.create({
+      component: UpdateTaskPage,
+      componentProps: { task: selectedTask }
+    })
+
+    modal.onDidDismiss().then(() => {
+      this.getAllTask()
+    })
+
+    return modal.present()
+  }
 }
---
+++ src/app/update-task/update-task.page.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { TodoService } from '../todo.service';
+
+@Component({
+  selector: 'app-update-task',
+  templateUrl: './update-task.page.html',
+  styleUrls: ['./update-task.page.scss'],
+})
+export class UpdateTaskPage implements OnInit {
+  @Input() task;
+  categories = []
+  categorySelectedCategory
+
+  newTaskObj = {}
+  itemName
+  itemDueDate
+  itemPriority
+  itemCategory
+
+  constructor(public modalCtlr: ModalController, public todoServive: TodoService) { }
+
+  ngOnInit() {
+    this.categories.push('work')
+    this.categories.push('personal')
+
+    this.itemName = this.task.value.itemName
+    this.itemDueDate = this.task.value.itemDueDate
+    this.itemPriority = this.task.value.itemPriority
+    this.categorySelectedCategory = this.task.value.itemCategory
+  }
+  selectCategory(index) {
+    this.categorySelectedCategory = this.categories[index]
+    console.log(this.categorySelectedCategory);
+  }
+
+  async dismis() {
+    await this.modalCtlr.dismiss()
+  }
+
+  async update() {
+    this.newTaskObj = ({ itemName: this.itemName, itemDueDate: this.itemDueDate, itemPriority: this.itemPriority, itemCategory: this.categorySelectedCategory })
+    let uid = this.task.key
+    this.todoServive.updateTask(uid, this.newTaskObj)
+    this.dismis();
+  }
+}
